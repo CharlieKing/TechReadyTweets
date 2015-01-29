d22b95ba8b42e114bce32f301344e10e237acb0e
Jason <github@jasonpoon.ca>
2015-01-28 17:24:38 -0800

    server

---
+++ server/routes/tweets.js
@@ -0,0 +1,35 @@
+﻿var express = require('express');
+var router = express.Router();
+var Tweet = require('../models/tweet');
+var nedb = require('nedb');
+var datastore = new nedb();
+
+/* GET users listing. */
+router.get('/', function (req, res) {
+    var tweets = datastore.getAllData().sort({ timestamp: -1 });
+    res.status(200).json({ tweets: tweets });
+});
+
+router.post('/', function (req, res, next) {
+    if (!req.body.tweet) {
+        return next({ status: 400 });
+    }
+
+    var author = req.body.tweet.author;
+    var message = req.body.tweet.message;
+
+    if (!author || !message) {
+        return next({ status: 400 });
+    }
+
+    var tweet = new Tweet(author, message);
+    datastore.insert(tweet, function (err) {
+        if (err) {
+            return next({ status: 500, message: err });
+        }
+
+        res.status(201).json({ tweet: tweet });
+    });
+});
+
+module.exports = router;
