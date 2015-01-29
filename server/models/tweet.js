d17e5ec668feea1ec1f5ab50b000fbc14fb79f8f
Jason <github@jasonpoon.ca>
2015-01-28 17:23:47 -0800

    Server

---
+++ server/models/tweet.js
@@ -0,0 +1,8 @@
+﻿var tweet = function (author, message) {
+    this.author = author;
+    this.message = message;
+    this.timestamp = Date.now().toString();
+    this.id = this.timestamp;
+}
+
+module.exports = tweet;
